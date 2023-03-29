387cdd2e3cc0912017c32985fff41ee3779848ad
Jorge <jrg.ledo@gmail.com>
2023-03-29 09:00:22 +0200

    Update Limersurveyguardar.R
    Crear función que realice todo el proceso

--- Limersurveyguardar.R
+++ Limersurveyguardar.R
@@ -2,46 +2,45 @@ library(limer)
 library(googlesheets4)
 library(tidyverse)
 
+# Leer las credenciales y URL de pass.txt
+credentials <- readLines("pass.txt")
+username <- credentials[1]
+password <- credentials[2]
+url <- credentials[3]
+url_gsheet <- credentials[4]
+mail <- credentials[5]
 
-#get username from the first lineof pass.txt
-username <- readLines("pass.txt")[1]
-password <- readLines("pass.txt")[2]
-url <- readLines("pass.txt")[3]
-url_gsheet <- readLines("pass.txt")[4]
-mail <- readLines("pass.txt")[5]
-
-#loguear en las apis
+# Iniciar sesión en las APIs
 options(lime_api = url)
 options(lime_username = username)
 options(lime_password = password)
 
-
-
-#key de la API de limesurvey
+# Obtener clave de sesión de la API de limesurvey
 get_session_key()
 
-#log en google sheets
+# Iniciar sesión en Google Sheets
 gs4_auth(email = mail)
 
-
-# dataframe con la lista de encuestas
-survey_df<-call_limer(method='list_surveys')
+# Dataframe con la lista de encuestas
+survey_df <- call_limer(method = 'list_surveys')
 print(survey_df)
 
-#replicar survey_title <- get_responses(iSurveyID= "sid") para cada encuesta
-corunha <- get_responses(iSurveyID= "375797")
-lancara <- get_responses(iSurveyID= "246285")
-lugo <- get_responses(iSurveyID= "953215")
-pastoriza <- get_responses(iSurveyID= "949626")
-guitiriz <- get_responses(iSurveyID= "851874")
+# Función para obtener respuestas y escribir en Google Sheets
+write_responses_to_sheet <- function(iSurveyID, sheet_name) {
+  responses <- get_responses(iSurveyID = iSurveyID)
+  range_write(responses, ss = url_gsheet, sheet = sheet_name, range = "A2", col_names = FALSE)
+}
 
+# Diccionario con IDs de encuesta y nombres de hojas
+survey_ids <- c("375797", "246285", "953215", "949626", "851874")
+sheet_names <- c("corunha", "lancara", "lugo", "pastoriza", "guitiriz")
 
-#importar a google sheets
-range_write(lancara, ss=url_gsheet, sheet = "lancara", range = "A2", col_names = FALSE)
-range_write(lugo, ss=url_gsheet, sheet = "lugo", range = "A2", col_names = FALSE)
-range_write(pastoriza, ss=url_gsheet, sheet = "pastoriza", range = "A2", col_names = FALSE)
-range_write(guitiriz, ss=url_gsheet, sheet = "guitiriz", range = "A2", col_names = FALSE)
-range_write(corunha, ss=url_gsheet, sheet = "corunha", range = "A2", col_names = FALSE)
+# Iterar sobre los IDs de encuesta y nombres de hojas, y escribir respuestas en Google Sheets
+for (i in seq_along(survey_ids)) {
+  write_responses_to_sheet(survey_ids[i], sheet_names[i])
+}
 
-#cerrar sesion
+write_responses_to_sheet(375797, "corunha")
+
+# Cerrar sesión
 release_session_key()
