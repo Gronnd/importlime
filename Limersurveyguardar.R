62d8d6a38a31813635ad379cca593d9ffd416a3b
Jorge <jrg.ledo@gmail.com>
2023-06-16 16:53:41 +0200

    Update Limersurveyguardar.R

--- Limersurveyguardar.R
+++ Limersurveyguardar.R
@@ -2,9 +2,6 @@ library(limer)
 library(googlesheets4)
 library(tidyverse)
 
-
-
-
 # Leer las credenciales y URL de pass.txt
 credentials <- readLines("pass.txt")
 username <- credentials[1]
@@ -12,11 +9,11 @@ password <- credentials[2]
 url <- credentials[3]
 mail <- credentials[4]
 
-# Datos para importaciones
+# Leer los datos para importaciones
 datos <- readLines("importaciones.txt")
-hoja <- datos[1]
-
-
+iSurveyID <- datos[1]
+sheet_name <- datos[2]
+url_gsheet <- datos[3]
 
 # Iniciar sesión en las APIs
 options(lime_api = url)
@@ -26,14 +23,8 @@ options(lime_password = password)
 # Obtener clave de sesión de la API de limesurvey
 get_session_key()
 
-
 # Iniciar sesión en Google Sheets
 gs4_auth(email = mail)
-
-# Dataframe con la lista de encuestas
-survey_df <- call_limer(method = 'list_surveys')
-print(survey_df)
-
 
 # Función para obtener respuestas y escribir en Google Sheets
 write_responses_to_sheet <- function(iSurveyID, sheet_name, url_gsheet) {
@@ -41,10 +32,12 @@ write_responses_to_sheet <- function(iSurveyID, sheet_name, url_gsheet) {
   range_write(responses, ss = url_gsheet, sheet = sheet_name, range = "A2", col_names = FALSE)
 }
 
-# Diccionario con IDs de encuesta y nombres de hojas
-
-write_responses_to_sheet("791131", "Agronovo_2023", hoja)
+# Llamar a la función con los datos leídos de importaciones.txt
+write_responses_to_sheet(iSurveyID, sheet_name, url_gsheet)
 
 # Cerrar sesión en la API de limesurvey
 release_session_key()
+
+# Limpiar el entorno de trabajo
 rm(list = ls())
+
